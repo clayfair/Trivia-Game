7ad4bd2a60fe292da2b3a2fc413a4a69ac7f26a7
Clay Fairbanks <clayfairbanks@Clays-MacBook-Pro.local>
2018-03-09 17:38:13 -0500

    one question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,13 @@
+//COUNTER IS 7 INSTEAD OF 30 FOR TESTING PURPOSES (QUICKER)
+var count = 7;
 
 window.onload = function () {
     $("#timeRemaining").hide();
     $(".questionOne").hide();
-    $(".answerOne").hide();
+    $(".answerOneTime").hide();
+    $(".answerOneCorrectChoice").hide();
+    $(".answerOneWrongChoice").hide();
+
     //start button needs to 
     //A) hide the start button HTML
     //B) begin the 30 sec timer 
@@ -13,6 +18,24 @@ window.onload = function () {
         displayQuestionOne();
         revealAnswer();
     });
+
+
+    //ON CLICK 
+    //right answer
+    $(".correct").click(function () {
+        $(".questionOne").hide();
+        $(".answerOneCorrectChoice").show();
+        count = -1;
+        
+    });
+
+    //wrong answer
+    $(".wrong").click(function () {
+        $(".questionOne").hide();
+        $(".answerOneWrongChoice").show();
+        count = -1;
+    });
+
 }
 
 //---------------------------------TIMER CODE--------------------------------------------------
@@ -20,8 +43,11 @@ window.onload = function () {
 function timer() {
     $("#timeRemaining").show();
 
+    function myStopFunction() {
+        clearInterval(myVar);
+    }
 
-    var count = 7;
+
     var myVar = setInterval(appendCount, 1000);
 
     function appendCount() {
@@ -29,13 +55,14 @@ function timer() {
             count--
             $("#timeRemaining").text("Time Remaining: " + count);
         } else {
-            stop();
+            myStopFunction();
+            revealAnswer();
         }
     }
 }
 
 
-//----------------------------------------QUESTION ONE-------------------------------------------
+//--------------------------------QUESTION ONE--------------------------------------------------
 
 //onClick we run this (display the question)
 function displayQuestionOne() {
@@ -43,19 +70,34 @@ function displayQuestionOne() {
 
 }
 
+//--------------------------------QUESTION ONE ANSWER -------------------------------------------
+
+//-----------VIA TIME RUNNING OUT-----------
 //set in motion a timeout to reveal the answer
 function revealAnswer() {
-    //in 7 seconds...
-    var newVar = setTimeout(questionOneAnswer, 1000 * 7);
-    //I want to empty questionOne and replace it with
-    function questionOneAnswer() {
+
+    if(count === 0) {
         $(".questionOne").hide();
-        $(".answerOne").show();
+        $(".answerOneTime").show();
     }
 }
 
+//-----------NEXT QUESTION (*******IGNORE THIS NOT RELEVANT TO GRADER*********)-----------
+  // function myStopFunction() {
+    //     clearInterval(myNewVar);
+    // }
+
+    // if (count > 0) {
+    //     //in 7 seconds...
+    //     var newVar = setTimeout(questionOneAnswer, 1000 * 7);
+    //     //I want to empty questionOne and replace it with
+    //     function questionOneAnswer() {
+    //         $(".questionOne").hide();
+    //         $(".answerOneTime").show();
+    //     }
+
+    // } else {
+    //     myStopFunction();
+    //}
 
 
-
-  //we can display one question, set interval to match (30 seconds), at the end of 30 seconds display the answer, set interval for
-    //5 seconds then display another question 
