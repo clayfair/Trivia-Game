8334ad648e433251344f562702c52f9c621e8890
Clay Fairbanks <clayfairbanks@Clays-MacBook-Pro.local>
2018-03-08 19:01:12 -0500

    countdown

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,17 +7,25 @@ window.onload = function () {
         $("#start").hide();
         //show the questions
         $("#questions").show();
-        //start a 30 second timer
-        var secondsLeft = 10;
-        var clock = setInterval(function () {
-            secondsLeft--
-            if (secondsLeft < 0) {
-                clearInterval(clock);
-            } else {
-                $("timeRemaining").append(secondsLeft.toString());
-            }
+        //display a 30 second timer. That entails... 
+        //starting at 30
+        var count = 30;
+        //counting down by 1
 
-        }, 1000)
+        //display count
+        //access the div containing text "Time Remaining: " and append the count 
+        // $("#timeRemaining").append(count);
+        //end at 0
+        //we want to append the count every second 
+        var myVar = setInterval(appendCount, 1000);
+
+        function appendCount() {
+            count--
+            $("#timeRemaining").text(count);
+        }
+
+
+
 
 
 
